543f874493130f6d265f0b5f488de182c045e743
Necrosz <g.alexisarellano@gmail.com>
2023-03-09 21:17:36 -0600

    final

--- src/components/DiscCard/DiscCard.jsx
+++ src/components/DiscCard/DiscCard.jsx
@@ -5,6 +5,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 
@@ -12,9 +19,9 @@ function DiscCard(disc) {
 
     const {name, image, id} = disc.disc
   return (
-    <div className='card'> 
+    <div className='card2'> 
     
-    <Card sx={{ maxWidth: 345 }} key={id}>
+    {/* <Card sx={{ maxWidth: 345 }} key={id}>
     <CardMedia
       sx={{ height: 250, width:300 }}
       image={image}
@@ -28,7 +35,34 @@ function DiscCard(disc) {
     <CardActions>
       <Button href={`/disc-track/${id}`} size="small" >Tracks</Button>
     </CardActions>
-  </Card></div>
+  </Card> */}
+
+
+   {/* <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 1200}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Disc Name</TableCell>
+            <TableCell align="center">Disc</TableCell>
+            <TableCell align="center"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody> */}
+            <TableRow
+              key={id}
+              sx={{ '&:last-child td, &:last-child th': { width: 400 } }}> 
+            <TableCell component="th" scope="row">
+                {name}
+              </TableCell>
+              <TableCell align="right"><img src={image} alt={name}/></TableCell>
+               <TableCell align="right">
+               <Button href={`/disc-track/${id}`} size="small" >Tracks</Button>
+               </TableCell>
+            </TableRow>
+        {/* </TableBody>
+      </Table>
+    </TableContainer> */}
+  </div>
   )
 }
 
--- src/pages/DiscDetails/DiscDetails.jsx
+++ src/pages/DiscDetails/DiscDetails.jsx
@@ -1,8 +1,17 @@
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 import DiscCard from '../../components/DiscCard/DiscCard'
 import { getDiscDetails } from '../../services/spotify.service'
 import "./DiscDetails.css"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export const getDiscLoader = async ({params}) => {
     const {data: disc} = await getDiscDetails(params.id)
@@ -12,13 +21,46 @@ export const getDiscLoader = async ({params}) => {
 function DiscDetails() {
 
     const {disc} = useLoaderData()
+    const [expanded, setExpanded] = React.useState('panel1');
 
   return (
     <div className='discdetails'>
     <div className="cardDisc">
-   { disc ? disc.map((disc) => (
+
+    <Accordion expanded={expanded === 'panel1'}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Discography</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 1200}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Disc Name</TableCell>
+            <TableCell align="center">Disc</TableCell>
+            <TableCell align="center"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow
+              key={disc._id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}> 
+            { disc ? disc.map((disc) => (
         <DiscCard key={disc.id} disc={disc} />
       )) : null}
+      </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+  
       </div>
       </div>
   )
